c1c0ab3bec6c7d61c2a6de5c1c036962d8df49ff
xshYYDS <98002367+xshYYDS@users.noreply.github.com>
2022-08-29 10:33:50 +0800

    Initial commit

---
+++ arduino/haucheng0828/haucheng0828.ino
@@ -0,0 +1,344 @@
+#include <MsTimer2.h> //定时器库的头文件
+#include <Wire.h>     //I2C Arduino Library
+#include <math.h>
+#define addr 0x1E // HMC5883的I2C地址,scl A5 ,sda A4
+
+int angle, serialData[4], i, temp = 0;
+
+//三个轮子，六根控制线
+int zuoqian = 10;
+int zuohou = 11;
+int youqian = 6;
+int youhou = 5;
+int zhouqian = 9;
+int zhouhou = 8;
+
+//三个轮子，六根反馈线
+int left0 = 2; //黄色
+int left1 = 4; //绿色
+int right0 = 7;
+int right1 = 3;
+int zhou0 = 12;
+int zhou1 = 13;
+
+// 电机反馈脉冲计数
+long number0 = 0, number1 = 0, number3 = 0;
+int rpm0, rpm1;
+int num = 1100; //轴转的单位数
+
+// PID参数设置
+double Input0, Input1, Output0, Output1, Setpoint0, Setpoint1;
+double errSum0, errSum1, lastErr0, lastErr1;
+// P主管响应，I减小静差，D抑制震荡。
+// p比例，p很小，柔软；p很大，生硬。
+// i积分，i很小，到不了目标值；i很大，超过目标值震荡。
+// d微分，d很小，围绕目标值震荡；d很大，很难达到目标值。
+double kp = 0.6, ki = 0.1, kd = 0.05;
+
+//初始化硬件设置
+void setup()
+{
+    //引脚模式设置
+    pinMode(left0, INPUT_PULLUP);
+    pinMode(right0, INPUT_PULLUP);
+    pinMode(left1, INPUT);
+    pinMode(right1, INPUT);
+    pinMode(zuoqian, OUTPUT);
+    pinMode(zuohou, OUTPUT);
+    pinMode(youqian, OUTPUT);
+    pinMode(youhou, OUTPUT);
+
+    //定时器，每 100ms触发一次，调用 read_speed（）函数
+    MsTimer2::set(100, read_speed);
+    MsTimer2::start();
+
+    //电机编码器反馈信号
+    attachInterrupt(digitalPinToInterrupt(left0), zuolun, FALLING);  // 2脚下降沿触发中断
+    attachInterrupt(digitalPinToInterrupt(right1), youlun, FALLING); // 3脚下降沿触发中断
+
+    // serial通信设置
+    Serial.begin(9600);
+
+    // HMC5883磁场编码器初始化
+    Wire.begin();
+    Wire.beginTransmission(addr);
+    Wire.write(0x02); //设置寄存器
+    Wire.write(0x00); //设置连续测量模式
+    Wire.endTransmission();
+}
+//主循环函数
+void loop()
+{
+    maganet();
+    Serial.print(angle, DEC);
+    Serial.print(",");
+    Serial.print(int(Setpoint0), DEC);
+    Serial.print(",");
+    Serial.print(int(Setpoint1), DEC);
+    Serial.print(",");
+    Serial.print(int(Output0), DEC);
+    Serial.print(",");
+    Serial.print(int(Output1), DEC);
+    Serial.print(",");
+    Serial.print(rpm0, DEC);
+    Serial.print(",");
+    Serial.print(rpm1, DEC);
+    while (1)
+    {
+        if (read_uart() == 1)
+        {
+            control();
+            break;
+        }
+        delay(10);
+    }
+}
+
+//读取UART缓冲区，读取速度。
+int read_uart()
+{
+    if (Serial.available() >= 14)
+    {
+        //检查串口缓存中是否有数据等待读取
+        //读取串口缓存中等待的字符,并赋值给字符变量serialData
+        for (i = 0; i < 4; i++)
+        {
+            serialData[i] = (Serial.read() - 48) * 100;
+            serialData[i] += (Serial.read() - 48) * 10;
+            serialData[i] += (Serial.read() - 48);
+        }
+        int trig0 = Serial.read() - 48;
+        int trig1 = Serial.read() - 48;
+        //清空缓冲区中的数据
+        while (Serial.available() != 0)
+        {
+            Serial.read();
+        }
+
+        if (serialData[0] != 0)
+        {
+            Setpoint0 = -serialData[0];
+        }
+        else
+        {
+            Setpoint0 = serialData[1];
+        }
+        if (serialData[2] != 0)
+        {
+            Setpoint1 = -serialData[2];
+        }
+        else
+        {
+            Setpoint1 = serialData[3];
+        }
+        if (Setpoint0 == 0)
+        {
+            analogWrite(zuoqian, 0);
+            analogWrite(zuohou, 0);
+            Input0 = 0, Output0 = 0, errSum0 = 0, lastErr0 = 0;
+        }
+        if (Setpoint1 == 0)
+        {
+            analogWrite(youqian, 0);
+            analogWrite(youhou, 0);
+            Input1 = 0, Output1 = 0, errSum1 = 0, lastErr1 = 0;
+        }
+        if (trig0)
+        {
+            trig0 = getball();
+            stop();
+            while(1){if(trig0==0){break;delay(100);}}
+        }
+        if (trig1)
+        {
+            trig1 = putball();
+            stop();
+            while(1){if(trig1==0){break;delay(100);}}
+        }
+        return 1;
+    }
+    return 0;
+}
+
+void stop()
+{
+    analogWrite(zuoqian, 0);
+    analogWrite(zuohou, 0);
+    analogWrite(youqian, 0);
+    analogWrite(youhou, 0);
+    Setpoint0 == 0, Setpoint1 == 0, Input1 = 0, Output1 = 0, errSum1 = 0, lastErr1 = 0, Input0 = 0, Output0 = 0, errSum0 = 0, lastErr0 = 0;
+}
+//使用PID产生的output控制电机
+void control()
+{
+    Input0 = rpm0;
+    Input1 = rpm1;
+    Compute0();
+    Compute1();
+    if (Output0 < 0)
+    {
+        if (Output0 < -255)
+        {
+            Output0 = -255;
+        }
+        analogWrite(zuohou, -Output0);
+        analogWrite(zuoqian, 0);
+    }
+    else
+    {
+        if (Output0 > 255)
+        {
+            Output0 = 255;
+        }
+        analogWrite(zuohou, 0);
+        analogWrite(zuoqian, Output0);
+    }
+    if (Output1 < 0)
+    {
+        if (Output1 < -255)
+        {
+            Output1 = -255;
+        }
+
+        analogWrite(youhou, -Output1);
+        analogWrite(youqian, 0);
+    }
+    else
+    {
+        if (Output1 > 255)
+        {
+            Output1 = 255;
+        }
+        analogWrite(youhou, 0);
+        analogWrite(youqian, Output1);
+    }
+}
+
+//左轮PID控制
+void Compute0()
+{
+    double error0 = Setpoint0 - Input0;
+    errSum0 += error0;
+    double dErr0 = error0 - lastErr0;
+    Output0 = kp * error0 + ki * errSum0 + kd * dErr0;
+    lastErr0 = error0;
+}
+
+//右轮PID控制
+void Compute1()
+{
+    double error1 = Setpoint1 - Input1;
+    errSum1 += error1;
+    double dErr1 = error1 - lastErr1;
+    Output1 = kp * error1 + ki * errSum1 + kd * dErr1;
+    lastErr1 = error1;
+}
+
+//读取磁场
+void maganet()
+{
+    //西0°，北130°，东195°，南256°
+    int x, y, z; // triple axis data
+    Wire.beginTransmission(addr);
+    Wire.write(0x03); // start with register 3.
+    Wire.endTransmission();
+
+    // Read the data.. 2 bytes for each axis.. 6 total bytes
+    Wire.requestFrom(addr, 6);
+    if (6 <= Wire.available())
+    {
+        x = Wire.read() << 8; // MSB  x
+        x |= Wire.read();     // LSB  x
+        z = Wire.read() << 8; // MSB  z
+        z |= Wire.read();     // LSB z
+        y = Wire.read() << 8; // MSB y
+        y |= Wire.read();     // LSB y
+    }
+
+    angle = (int(atan2((double)y, (double)x) * (180 / 3.14159265) + 180)); // angle in degrees
+    String angle_str = String(angle);
+}
+
+//产生速度函数
+void read_speed()
+{
+    rpm0 = int(number0 / 0.39); //减速比21.3，编码器精度11，num/11/21.3*120 num/11/21.3*10*60
+    number0 = 0;
+    rpm1 = int(number1 / 0.39);
+    number1 = 0;
+}
+
+//左轮编码器脉冲计数中断函数
+void zuolun()
+{
+    if (digitalRead(left0) == LOW)
+    {
+        if (digitalRead(left1) == LOW) // 查询EN_B的电平以确认正转
+        {
+            number0--;
+        }
+        if (digitalRead(left1) == HIGH) // 查询EN_B的电平以确认反转
+        {
+            number0++;
+        }
+    }
+}
+//右轮编码器脉冲计数中断函数
+void youlun()
+{
+    if (digitalRead(right1) == LOW)
+    {
+        if (digitalRead(right0) == LOW) // 查询EN_B的电平以确认正转
+        {
+            number1++;
+        }
+        if (digitalRead(right0) == HIGH) // 查询EN_B的电平以确认反转
+        {
+            number1--;
+        }
+    }
+}
+
+//抓球
+int getball()
+{
+    digitalWrite(zhouqian, HIGH);
+    digitalWrite(zhouhou, LOW);
+    while (1)
+    {
+        if (digitalRead(zhou0) != temp)
+        {
+            temp = digitalRead(zhou0);
+            number3++;
+        };
+        if (number3 == num)
+        {
+            number3 = 0;
+            digitalWrite(zhouqian, 0);
+            digitalWrite(zhouhou, 0);
+            return 0;
+        };
+    }
+}
+
+//放球
+int putball()
+{
+    digitalWrite(zhouqian, LOW);
+    digitalWrite(zhouhou, HIGH);
+    while (1)
+    {
+        if (digitalRead(zhou0) != temp)
+        {
+            temp = digitalRead(zhou0);
+            number3++;
+        };
+        if (number3 == num)
+        {
+            number3 = 0;
+            digitalWrite(zhouqian, 0);
+            digitalWrite(zhouhou, 0);
+            return 0;
+        };
+    }
+}
