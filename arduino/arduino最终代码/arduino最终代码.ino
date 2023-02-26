fb8ec5dec57f491ea0013071d8e0d41de04cd7f8
xsh <2726841514@qq.com>
2023-02-26 20:08:02 +0800

    0226

---
+++ arduino/arduino最终代码/arduino最终代码.ino
@@ -0,0 +1,290 @@
+#include <MsTimer2.h> //定时器库的头文件
+#include <Wire.h>     //I2C Arduino Library
+#include <math.h>
+#define addr 0x1E // HMC5883的I2C地址,scl A5 ,sda A4
+
+int angle, pwm[4], i, temp = 0;
+
+//三个轮子，六根控制线
+int zuoqian = 10;
+int zuohou = 11;
+int youqian = 6;
+int youhou = 5;
+int zhouqian = 9;
+int zhouhou = 8;
+
+//三个轮子，六根反馈线
+int left0 = 2; //黄色
+int left1 = 4; //绿色
+int right0 = 7;
+int right1 = 3;
+int zhou0 = 12;
+int zhou1 = 13;
+
+// 电机反馈脉冲计数
+long number0 = 0, number1 = 0, number3 = 0;
+int rpm0 = 0, rpm1 = 0;
+int num = 1100; //轴转的单位数
+
+//初始化硬件设置
+void setup()
+{
+    //引脚模式设置
+    pinMode(left0, INPUT_PULLUP);
+    pinMode(right0, INPUT_PULLUP);
+    pinMode(left1, INPUT);
+    pinMode(right1, INPUT);
+    pinMode(zuoqian, OUTPUT);
+    pinMode(zuohou, OUTPUT);
+    pinMode(youqian, OUTPUT);
+    pinMode(youhou, OUTPUT);
+    pinMode(zhouqian, OUTPUT);
+    pinMode(zhouhou, OUTPUT);
+    //定时器，每 50ms触发一次，调用 read_speed（）函数
+    MsTimer2::set(50, read_speed);
+    MsTimer2::start();
+
+    //电机编码器反馈信号
+    attachInterrupt(digitalPinToInterrupt(left0), zuolun, FALLING);  // 2脚下降沿触发中断
+    attachInterrupt(digitalPinToInterrupt(right1), youlun, FALLING); // 3脚下降沿触发中断
+
+    // serial通信设置
+    Serial.begin(9600);
+
+    // HMC5883磁场编码器初始化
+    Wire.begin();
+    Wire.beginTransmission(addr);
+    Wire.write(0x02); //设置寄存器
+    Wire.write(0x00); //设置连续测量模式
+    Wire.endTransmission();
+
+}
+//主循环函数
+void loop()
+{
+    maganet(); 
+    Serial.print("a");
+    Serial.print(",");
+    Serial.print(angle, DEC);
+    Serial.print(",");
+    Serial.print(rpm0, DEC);
+    Serial.print(",");
+    Serial.print(rpm1, DEC);
+    Serial.print(",");
+    for (i = 0; i < 4; i++)
+    {
+        Serial.print(pwm[i]);
+        Serial.print(",");
+    }
+    Serial.print("b");
+    Serial.print(",");
+    control();
+
+}
+
+//读取UART缓冲区，读取速度。
+void control()
+{
+  while (1)
+    { //检查串口缓存中是否有16位数据等待读取
+    if (Serial.available() >= 16)
+        {
+            //判断起始位是否是a
+            if (Serial.read() != 97)
+            {
+                //清空缓冲区中的数据
+                while (Serial.available() != 0)
+                {
+                    Serial.read();
+                }
+                return;
+            }
+            //读取串口缓存中等待的字符,并赋值给字符变量pwm
+            for (i = 0; i < 4; i++)
+            {
+                pwm[i] = (Serial.read() - 48) * 100;
+                pwm[i] += (Serial.read() - 48) * 10;
+                pwm[i] += (Serial.read() - 48);
+            }
+            int trig0 = Serial.read() - 48;
+            int trig1 = Serial.read() - 48;
+            //判断结束位是否是b
+            if (Serial.read() != 98)
+            {
+                //清空缓冲区中的数据
+                while (Serial.available() != 0)
+                {
+                    Serial.read();
+                }
+                return;
+            }
+
+            if (pwm[0] != 0)
+            {
+                analogWrite(zuohou, pwm[0]);
+                analogWrite(zuoqian, 0);
+            }
+            else if (pwm[1] != 0)
+            {
+                analogWrite(zuoqian, pwm[1]);
+                analogWrite(zuohou, 0);
+            }
+            else
+            {
+                analogWrite(zuohou, 0);
+                analogWrite(zuoqian, 0);
+            }
+            if (pwm[2] != 0)
+            {
+                analogWrite(youhou, pwm[2]);
+                analogWrite(youqian, 0);
+            }
+            else if (pwm[3] != 0)
+            {
+                analogWrite(youqian, pwm[3]);
+                analogWrite(youhou, 0);
+            }
+            else
+            {
+                analogWrite(youhou, 0);
+                analogWrite(youqian, 0);
+            }
+            if (trig0)
+            {
+                trig0 = getball();
+                while (1)
+                {
+                    if (trig0 == 0)
+                    {
+                        break;
+                    }
+                    delay(10);
+                }
+            }
+            if (trig1)
+            {
+                trig1 = putball();
+                while (1)
+                {
+                    if (trig1 == 0)
+                    {
+                        break;
+                    }
+                    delay(10);
+                }
+            }
+            break;
+        }
+
+        delay(10);
+    }
+}
+
+//读取磁场
+void maganet()
+{
+    //西0°，北130°，东195°，南256°
+    int x, y, z; // triple axis data
+    Wire.beginTransmission(addr);
+    Wire.write(0x03); // start with register 3.
+    Wire.endTransmission();
+
+    // Read the data.. 2 bytes for each axis.. 6 total bytes
+    Wire.requestFrom(addr, 6);
+    if (6 <= Wire.available())
+    {
+        x = Wire.read() << 8; // MSB  x
+        x |= Wire.read();     // LSB  x
+        z = Wire.read() << 8; // MSB  z
+        z |= Wire.read();     // LSB z
+        y = Wire.read() << 8; // MSB y
+        y |= Wire.read();     // LSB y
+    }
+
+    angle = (int(atan2((double)y, (double)x) * (180 / 3.14159265) + 180)); // angle in degrees
+    String angle_str = String(angle);
+}
+
+//产生速度函数
+void read_speed()
+{
+    rpm0 = int(number0 / 0.2); //减速比21.3，编码器精度11， num/11/21.3*20*60约等于0.195
+    number0 = 0;
+    rpm1 = int(number1 / 0.2);
+    number1 = 0;
+}
+
+//左轮编码器脉冲计数中断函数
+void zuolun()
+{
+    if (digitalRead(left0) == LOW)
+    {
+        if (digitalRead(left1) == LOW) // 查询EN_B的电平以确认正转
+        {
+            number0--;
+        }
+        if (digitalRead(left1) == HIGH) // 查询EN_B的电平以确认反转
+        {
+            number0++;
+        }
+    }
+}
+//右轮编码器脉冲计数中断函数
+void youlun()
+{
+    if (digitalRead(right1) == LOW)
+    {
+        if (digitalRead(right0) == LOW) // 查询EN_B的电平以确认正转
+        {
+            number1++;
+        }
+        if (digitalRead(right0) == HIGH) // 查询EN_B的电平以确认反转
+        {
+            number1--;
+        }
+    }
+}
+
+//抓球
+int getball()
+{
+    digitalWrite(zhouqian, HIGH);
+    digitalWrite(zhouhou, LOW);
+    while (1)
+    {
+        if (digitalRead(zhou0) != temp)
+        {
+            temp = digitalRead(zhou0);
+            number3++;
+        };
+        if (number3 == num)
+        {
+            number3 = 0;
+            digitalWrite(zhouqian, 0);
+            digitalWrite(zhouhou, 0);
+            return 0;
+        };
+    }
+}
+
+//放球
+int putball()
+{
+    digitalWrite(zhouqian, LOW);
+    digitalWrite(zhouhou, HIGH);
+    while (1)
+    {
+        if (digitalRead(zhou0) != temp)
+        {
+            temp = digitalRead(zhou0);
+            number3++;
+        };
+        if (number3 == num)
+        {
+            number3 = 0;
+            digitalWrite(zhouqian, 0);
+            digitalWrite(zhouhou, 0);
+            return 0;
+        };
+    }
+}
---
+++ maixpy/maixpy最终版.py
@@ -0,0 +1,439 @@
+import image, utime, time, gc, sensor,Maix,sys,random
+from fpioa_manager import fm
+from Maix import GPIO, I2S
+from machine import Timer, UART
+import KPU as kpu
+
+
+
+# ————————————————————————————————————————-———————————————————————————————————————————————————
+# 全局变量
+# ———————————————————————————————————————————————————————————————————————————————————————————
+# 语音命令转换成磁场角[1:北边,2:东北,3:东边,4:东南,5:南边,6:西南,7:西边,8:西北]
+dir2mag = [100, 75, 75, 83, 95, 110, 120, 120]
+# 全局变量磁场角度
+mag = 0
+# 左右轮转速
+rpm1, rpm0 = 0, 0
+turn_info = 0
+# 循迹参数设置
+threshold=(100,255)
+green=(10,45,-16,-7,12,22)
+#ROI = (0,20,32,12)
+ROI=[80, 90, 130, 120]
+kernel_size = 1
+kernel = [-1, -1, -1,
+          -1, +8, -1,
+          -1, -1, -1]
+# ————————————————————————————————————
+# 设置摄像头参数
+# ————————————————————————————————————
+input_size = (224, 224)
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)  # 设置摄像机的像素格式
+sensor.set_framesize(sensor.QVGA)
+sensor.set_windowing(input_size)
+sensor.set_hmirror(False)  # 打开水平镜像模式
+sensor.set_vflip(False)  # 打开竖直反转模式
+sensor.skip_frames(time=2000)
+sensor.run(1)
+
+
+# ——————————————————————————————————————————————————————————————————————————————————————————
+# 通信接口设置Uart，共有三个，1~3，uart UART 号，波特率,UART 数据宽度，奇偶校验位，停止位,串口接收超时时间,串口接收缓冲
+# ——————————————————————————————————————————————————————————————————————————————————————————
+#连接arduino模块
+fm.register(0, fm.fpioa.UART1_TX, force=True)
+fm.register(1, fm.fpioa.UART1_RX, force=True)
+uart1 = UART(UART.UART1, 115200, 8, None, 0, timeout=1000, read_buf_len=4096)
+# 语音识别uart
+fm.register(2, fm.fpioa.UART2_TX, force=True)
+fm.register(3, fm.fpioa.UART2_RX, force=True)
+uart2 = UART(UART.UART2, 9600, 8, 1, 0, timeout=1000, read_buf_len=4096)
+#连接无线通讯模块，esp32,34RX，35TX
+fm.register(0, fm.fpioa.UART1_TX, force=True)
+fm.register(1, fm.fpioa.UART1_RX, force=True)
+uart3 = UART(UART.UART3, 115200, 8, None, 0, timeout=1000, read_buf_len=4096)
+
+#接收cc1101数据
+def cc1101():
+    while (1):
+        data = uart3.read()
+        if (data != None):
+            data = str(data)
+            print("数据：",data)
+            return data
+        utime.sleep_ms(100)
+
+
+
+# 测距模块
+fm.register(8, fm.fpioa.GPIO0)  # trig触发信号
+fm.register(9, fm.fpioa.GPIO1)  # echo回响信号
+trig = GPIO(GPIO.GPIO0, GPIO.OUT)
+echo = GPIO(GPIO.GPIO1, GPIO.IN)
+
+
+# ————————————————————————————
+# 读取语音指令
+# ————————————————————————————
+def listen():
+    while (1):
+        data = uart2.read()
+        if (data != None):
+            data = str(data)[10:-6]
+            direction = dir2mag[int(data) - 1]
+            print("语音指令方向磁场角：", direction)
+            return direction
+        utime.sleep_ms(100)
+
+# 定时器回调函数，使用定时器接收arduino反馈信号，可以保证maxipy忙碌时正常接收信号。
+def arduino(self):
+    global mag, rpm0, rpm1
+    read_data = uart1.read()
+    if (read_data != None):
+        data = str(read_data)[2:-1]
+        data = data.split(',')
+        try:
+            data = data[data.index("a") + 1:data.index("b")]
+            mag = int(data[0])
+            rpm0, rpm1 = 4*int(data[1]), 4*int(data[2])
+            print('磁场度数:%3s' % data[0], '真实速度:%4s' % rpm0, '%4s' % rpm1, 'pwm%3s' % data[3], '%3s' % data[4], '%3s' % data[5], '%3s' % data[6])
+        except:
+            pass
+
+
+# 与arduino通信，100ms自动通信一次
+tim = Timer(Timer.TIMER0, Timer.CHANNEL0, mode=Timer.MODE_PERIODIC, period=50, callback=arduino)
+tim.start()
+
+
+# ——————————————————————————————————————————————————————————————————————————————————————————————
+# 向Arduino发送速度数据,turn_info偏转量，范围为（-1,1），负为左转弯，正为右转弯，乘以100加到speed上
+# ————————————————————————————————————————————————————————————————————————————————————————
+# PID控制类
+# P主管响应，I减小静差，D抑制震荡。
+# p比例，p很小，柔软；p很大，生硬。
+# i积分，i很小，到不了目标值；i很大，超过目标值震荡。
+# d微分，d很小，围绕目标值震荡；d很大，很难达到目标值。
+#下面是三种PID实现方式
+
+
+#class PID():
+    #def __init__(self, name):
+        ## PID参数设置
+        #self.kp, self.ki = 3, 0.6
+        #self.lastpwm=0
+        #self.name = name
+
+    #def run(self, Set_rpm, real_rpm):
+        #error = Set_rpm - real_rpm
+        #pwm = int(self.kp * error + self.ki*self.lastpwm)
+        #self.lastpwm = pwm
+        #if (pwm > 255):
+            #pwm = 255
+        #if (pwm < -255):
+            #pwm = -255
+        ##print('%s' % self.name, '速度设定值:%4s' % Set_rpm, '真实速度:%4s' % real_rpm, '电机占空比%4s' % pwm)
+        #return pwm
+
+#class PID():
+    #def __init__(self, name):
+        ## PID参数设置
+        #self.kp, self.ki, self.kd = 0.6, 0.1, 0.05
+        #self.errSum, self.lastErr = 0, 0
+        #self.name = name
+
+    #def run(self, Set_rpm, real_rpm):
+        #error = Set_rpm - real_rpm
+        #self.errSum += error
+        #dErr = error - self.lastErr
+        #pwm = int(self.kp * error + self.ki * self.errSum + self.kd * dErr)
+        #self.lastErr = error
+        #if (pwm > 255):
+            #pwm = 255
+        #if (pwm < -255):
+            #pwm = -255
+        ##print('%s' % self.name, '速度设定值:%4s' % Set_rpm, '真实速度:%4s' % real_rpm, '电机占空比%4s' % pwm)
+        #return pwm
+
+
+class PID():
+    def __init__(self, name):
+        # PID参数设置
+        self.kp, self.ki, self.kd = 0.6, 0.1, 0.05
+        self.Output_duty,self.error,self.lastErr0,self.last_last_Err0,self.pwm=0,0,0,0,0
+        self.name = name
+
+    def run(self, Set_rpm, real_rpm):
+        error = Set_rpm - real_rpm
+        dErr0 = self.error - self.lastErr0
+        self.Output_duty = int(self.kp * dErr0 + self.ki * error + self.kd * (error - 2 * self.lastErr0 + self.last_last_Err0))
+        self.pwm += self.Output_duty
+        self.lastErr0 = self.error
+        self.last_last_Err0 = self.lastErr0
+        if (self.pwm > 255):
+            self.pwm = 255
+        if (self.pwm < -255):
+            self.pwm = -255
+        #print('%s' % self.name, '速度设定值:%4s' % Set_rpm, '真实速度:%4s' % real_rpm, '电机占空比%4s' % pwm)
+        return self.pwm
+
+def speed(turn_info=0, v0=100, det_v=150):
+    global rpm0, rpm1
+    if (turn_info < 0):
+        zuopwm = zuopid.run(v0, rpm0)
+        youpwm = youpid.run(v0 - turn_info * det_v, rpm1)
+    else:
+        zuopwm = zuopid.run(v0 + turn_info * det_v, rpm0)
+        youpwm = youpid.run(v0, rpm1)
+    to_arduino(zuopwm, youpwm)
+
+
+
+
+#向arduino发送信号函数
+def to_arduino(zuopwm, youpwm):
+    data = 'a'
+    if (zuopwm < 0):
+        data += '%03d' % -zuopwm + "000"
+    else:
+        data += "000" + '%03d' % zuopwm
+    if (youpwm < 0):
+        data += '%03d' % -youpwm + "000"
+    else:
+        data += "000" + '%03d' % youpwm
+    data += "00b"
+    uart1.write(data)
+
+
+#实例化两个轮子，分别代表左轮和右轮
+zuopid = PID("左轮")
+youpid = PID("右轮")
+
+
+#定义了三条指令，绕过PID直接控制arduino
+def stop():
+    uart1.write("a00000000000000b")
+def getball():
+    uart1.write("a00000000000010b")
+def putball():
+    uart1.write("a00000000000001b")
+
+
+
+def timer_speed(self):
+    v0=150
+    det_v=100
+    global rpm0, rpm1,turn_info
+    if (turn_info < 0):
+        zuopwm = zuopid.run(v0, rpm0)
+        youpwm = youpid.run(v0 - turn_info * det_v, rpm1)
+    else:
+        zuopwm = zuopid.run(v0 + turn_info * det_v, rpm0)
+        youpwm = youpid.run(v0, rpm1)
+    to_arduino(zuopwm, youpwm)
+#使用定时器发送速度信号，在maxipy忙时保证速度信号正常运行
+    timer = Timer(Timer.TIMER1, Timer.CHANNEL0, mode=Timer.MODE_PERIODIC, period=50, callback=timer_speed)
+
+# ————————————————————————————————————
+# 采集数据，拍摄图片，保存至SD卡
+# ————————————————————————————————————
+def photo():
+    number = random.randint(0, 10000000)
+    sensor.snapshot().save("/sd/" + str(number) + ".jpg")
+    print(number)
+
+
+# ————————————————————————————————
+# 测距, 单位为厘米
+# ————————————————————————————————
+def distance():
+    trig.value(1)
+    utime.sleep_us(10)
+    trig.value(0)
+    while not echo.value():
+        pass
+    t1 = time.ticks_us()
+    while echo.value():
+        pass
+    t2 = time.ticks_us()
+    return int((t2 - t1) * 0.034 / 2)
+
+
+# ————————————————————————————
+# 循迹
+# ————————————————————————————
+def detect_line():
+    #img = sensor.snapshot().to_grayscale().morph(kernel_size, kernel).binary([(100,255)]).erode(1, threshold = 2)
+    #img.morph(kernel_size, kernel)
+    #img.binary([(60,145)])
+    #img.erode(1, threshold = 2)
+    #line = img.get_regression([(100,255)], roi=ROI,robust = True)
+
+    img1 = sensor.snapshot()  # 正常读图片，为了找绿色做处理
+    img1 = img1.resize(32,32)
+    green_blobs = img1.find_blobs([green], merge=True)  # 所有的绿色块
+    green_pixels = 0
+    if (len(green_blobs)):
+        for green_blob in green_blobs:
+            green_pixels += green_blob.pixels()
+    print("绿色：",green_pixels)
+    #if(line):
+        #rho_err = (abs(line.rho())-img.width()/2) / 56
+        ##print(rho_err,line.magnitude(),rho_err)
+        #return rho_err
+    return 0
+
+# ————————————————————————————————————————————————————————————————————————————————————————————————————
+# 目标检测参数设置
+# ——————————————————————————————————————————————————————————————————————————————————————————————————————
+
+labels = ['people', 'goal', 'football']
+people = 0
+football = 2
+goal = 1
+anchors = [0.44, 0.47, 5.34, 3.62, 2.28, 1.94, 0.75, 1.06, 0.81, 2.97]
+task = None
+## 显示堆内存
+#print(Maix.utils.heap_free() / 1024)
+## 显示栈内存
+#print(gc.mem_free() / 1024)
+task = kpu.load("/sd/model-11393.kmodel")
+kpu.init_yolo2(task, 0.5, 0.3, 5, anchors)
+## 显示堆内存
+#print(Maix.utils.heap_free() / 1024)
+## 显示栈内存
+#print(gc.mem_free() / 1024)
+obs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+# —————————————————————————————————————————————————————————————————————————————————————————————————————
+# 目标检测,obs=[[football,置信度,纵坐标，横坐标],[goal,置信度，纵坐标，横坐标]，[people,置信度，纵坐标，横坐标]，……]
+# 示例：obs=[[1,0.8,43,91],[0,0.6,83,92],[2,0.7,38,29],[0,0.9,48,99]]
+# ——————————————————————————————————————————————————————————————————————————————————————————————————————
+def yolo():
+    img = sensor.snapshot()
+    objects = kpu.run_yolo2(task, img)
+    obs = []
+    if objects:
+        for i in objects:
+            obs.append([i.classid(), i.value(), i.y() - i.h(), (i.x() + i.w() / 2 - 112)])
+    return obs
+
+
+# ——————————————————————————————————————————————
+# ——————————————————————————————————————————————
+# ——————————————————————————————————————————————
+# ——————————————————————————————————————————————
+
+
+
+#开始运行
+timer.start()
+
+
+
+
+# ————————————————————————————————————————
+# 等待用户下达开始命令
+# ————————————————————————————————————————
+direction = listen()
+
+# ————————————————————————————————————————
+# 调整初始方向
+# ————————————————————————————————————————
+#提供一个初始速度，跑起来
+for i in range(4):
+    uart1.write("a00025500025500b")
+    utime.sleep_ms(100)
+
+#朝向用户指令方向
+while (1):
+    dif = direction - mag
+    print("磁场差值:", dif)
+    if (dif < 2 and dif > -2):
+        break
+    elif(dif>0):
+        uart1.write("a00025500000000b")
+    else:
+        uart1.write("a00000000025500b")
+
+
+# ——————————————————————————————————————————
+# 目标检测、避障
+# ——————————————————————————————————————————
+is_football=Flase
+while(1):
+    obs = yolo()
+    for i in range(len(obs)):
+        if (obs[i][0] == football):
+            is_football=True
+            break
+    if(is_football):
+        break
+
+while (1):
+    # 测距
+    dist = distance()
+    print(dist)
+    if (dist <= 5):
+        getball()
+        break
+    # 目标检测
+
+    obs = yolo()
+    for i in range(len(obs)):
+        if (obs[i][0] == football):
+            turn_info = obs[i][3] / 112
+            print("识别结果：", obs, turn_info)
+            break
+    speed(turn_info)
+
+# ————————————————————————————————————————————
+# 寻找足球门框，目标检测
+# ————————————————————————————————————————————
+flag = False
+while (1):
+    speed(-1)
+    obs = yolo()
+    for i in range(len(obs)):
+        if (obs[i][0] == goal):
+            stop()
+            flag = True
+            break
+    if (flag):
+        break
+
+while (1):
+    dist = distance()
+    if (dist <= 5):
+        getball()
+        break
+    speed(0)
+# ————————————————————————————————————————————
+# 走向足球门框，目标检测，避障
+# ————————————————————————————————————————————
+
+
+# ————————————————————————————————————————————
+# 放球
+# ————————————————————————————————————————————
+putball()
+
+print("结束任务")
+
+
+
+
+# 测试帧率
+# t = time.ticks_ms()
+# t = time.ticks_ms() - t
+# print(t)
+
+
+# 显示堆内存
+# print(Maix.utils.heap_free() / 1024)
+# 显示栈内存
+# print(gc.mem_free() / 1024)
+
